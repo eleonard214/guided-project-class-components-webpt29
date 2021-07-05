0eb76e29f3f29801009bbb47da818c262aa5505b
Warren Longmire <warrenlongmire@gmail.com>
2021-07-05 12:28:50 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Person = (props) => {
   return (<div>
     <h1>Hello {props.name}.</h1>
-    <h1>That is a great name {props.name}! Never change your name, {name}.</h1>
+    <h1>That is a great name {props.name}! Never change your name, {props.name}.</h1>
     <h2>Are you {props.age}?</h2>
   </div>);
 }
@@ -19,6 +19,8 @@ const App = () => {
 
   return (
     <div>
+      <Person name={name} age={age} />
+      <Person name={name} age={age} />
       <Person name={name} age={age} />
       <button onClick={handleClick}>Change the name</button>
     </div>
