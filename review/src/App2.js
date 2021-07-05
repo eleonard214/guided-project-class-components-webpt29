8848362aff8820243cb6f95d01101c48505158c0
Warren Longmire <warrenlongmire@gmail.com>
2021-07-05 12:55:34 -0400

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 class App2 extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Warren",
+      age: 35
+    }
+  }
+
   render() {
-    const name = "Warren";
     return (<h1>Hello {name}.</h1>);
   }
 }
