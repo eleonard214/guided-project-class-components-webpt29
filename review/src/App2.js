34dd4ea410be528edb64c87d22e549310964cc42
Warren Longmire <warrenlongmire@gmail.com>
2021-07-05 12:59:30 -0400

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -9,17 +9,21 @@ class App2 extends React.Component {
     }
   }
 
+  handleClick = () => {
+    this.setState({
+      ...this.state,
+      name: "Allison",
+      age: 12
+    });
+  }
+
   render() {
     return (<div>
       <h1>Hello {this.state.name}.</h1>
       <h2>Hello {this.state.age}.</h2>
       <button onClick={
         () => {
-          this.setState({
-            ...this.state,
-            name: "Allison",
-            age: 12
-          });
+
         }
       }>Change the name</button>
     </div>);
