2e236f254b06f65586f0ac483dc235338cc07b57
Warren Longmire <warrenlongmire@gmail.com>
2021-07-05 13:03:29 -0400

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -7,6 +7,14 @@ import React from 'react';
 //   </div>);
 // }
 
+class Person extends React.Component {
+  render() {
+    return (<div>
+      <h1>Hello {props.name}.</h1>
+      <h2>Hello {props.age}.</h2>
+    </div>);
+  }
+}
 
 class App2 extends React.Component {
   constructor() {
