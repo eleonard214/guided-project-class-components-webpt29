0e1b9b0b0051e29222de9421159092260d5accc9
Warren Longmire <warrenlongmire@gmail.com>
2021-07-05 13:28:30 -0400

    lecture work-in-progress

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -1,25 +1,23 @@
 import React, { useState } from 'react';
 
 const Item = props => {
-  const [item, setItem] = useState({
-    name: 'Bananas',
-    id: 123,
-    purchased: false
-  });
+  // const [item, setItem] = useState({
+  //   name: 'Bananas',
+  //   id: 123,
+  //   purchased: false
+  // });
 
   const handleClick = () => {
-    console.log("item: " + item.name);
-    //change purchased to true
-    setItem({
-      ...item,
-      // purchased: item.purchased ? false : true
-      purchased: !item.purchased
-    });
+    console.log("item: " + props.item.name);
+    // setItem({
+    //   ...item,
+    //   purchased: !item.purchased
+    // });
   }
 
   return (
-    <div onClick={handleClick} className={`item${item.purchased ? ' purchased' : ''}`}>
-      <p>{item.name}</p>
+    <div onClick={handleClick} className={`item${props.item.purchased ? ' purchased' : ''}`}>
+      <p>{props.item.name}</p>
     </div>
   );
 };
